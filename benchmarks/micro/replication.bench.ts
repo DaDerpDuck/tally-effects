0b6af35bd7b0bfb7270c0ce0cb4f640b694fd4b2
DaDerpDuck <14254403+DaDerpDuck@users.noreply.github.com>
2026-09-08 14:15:10 +0300

    Fix merge errors

--- benchmarks/micro/replication.bench.ts
+++ benchmarks/micro/replication.bench.ts
@@ -130,6 +130,7 @@ for (const size of [1, 10, 100, 1_000]) {
 		descriptors: Array.from({ length: size }, (_, index) => ({
 			id: index + 1,
 			type: "BenchmarkDescriptor",
+			key: undefined,
 			data: value,
 		})),
 	});
